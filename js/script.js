4c64f39b7986a2033a8a72e8f753aa4a7a9b1e67
tle2-npu <tle2@northpark.edu>
2025-11-11 10:50:36 -0600

    Add Giphy API key setup and endpoint configuration

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("script.js loaded");
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=tACXoZ7Sg3YwaD1NqcpLUkqSLcTQGDFu&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
